476c552ac4729e86f4e6dd6aa9b11aa1bfa4bf57
Callen <callenrwessels@gmail.com>
2023-11-20 22:06:45 -0600

    Adding SpellLevelSelector

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { SpellLevelSelector } from '@/components/SpellLevelSelector/SpellLevelSelector';
 
 export default function HomePage() {
   return (
     <>
+      <SpellLevelSelector/>
       <Welcome />
       <ColorSchemeToggle />
     </>
