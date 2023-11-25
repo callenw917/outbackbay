4e4ec2105cbcb421c6a8a0d09f9ef2a345becab1
Callen <callenrwessels@gmail.com>
2023-11-25 09:20:24 -0600

    One last time.

--- app/api/get-spells/route.ts
+++ app/api/get-spells/route.ts
@@ -11,5 +11,14 @@ export async function GET(request: Request)
         spells.push(new Spell(rawSpell.name, rawSpell.details, rawSpell.level));
     });
 
-    return Response.json(spells);
+    return Response.json(spells, {
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers":
+            "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+          "Access-Control-Max-Age": "86400",
+        }
+    });
 }
