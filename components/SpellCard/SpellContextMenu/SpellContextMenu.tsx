5f80f59637397461ef605e1cccb8d26d8f9605f9
Callen Wessels <callenrwessels@gmail.com>
2024-06-25 22:17:30 -0500

    Add a tooltip!

--- components/SpellCard/SpellContextMenu/SpellContextMenu.tsx
+++ components/SpellCard/SpellContextMenu/SpellContextMenu.tsx
@@ -1,4 +1,4 @@
-import { Button, Menu, rem } from '@mantine/core';
+import { Button, Menu, Tooltip, rem } from '@mantine/core';
 import { IconPlus, IconSearch } from '@tabler/icons-react';
 import { useState } from 'react';
 
@@ -7,23 +7,25 @@ export default function SpellContextMenu() {
 
   return (
     <Menu onOpen={() => setIsMenuOpen(true)} onClose={() => setIsMenuOpen(false)}>
-      <Menu.Target>
-        <Button
-          className="spellContextButton"
-          style={{ display: isMenuOpen ? 'block' : '' }}
-          size="xs"
-          radius="xl"
-          variant="outline"
-          color="green"
-          onClick={(event) => {
-            event.stopPropagation();
-          }}
-        >
-          <IconPlus className="spellContextButtonIcon"></IconPlus>
-        </Button>
-      </Menu.Target>
+      <Tooltip label="Add to a Spell List" color="green" withArrow>
+        <Menu.Target>
+          <Button
+            className="spellContextButton"
+            style={{ display: isMenuOpen ? 'block' : '' }}
+            size="xs"
+            radius="xl"
+            variant="outline"
+            color="green"
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
+            <IconPlus className="spellContextButtonIcon"></IconPlus>
+          </Button>
+        </Menu.Target>
+      </Tooltip>
 
-      <Menu.Dropdown>
+      <Menu.Dropdown miw={rem(200)}>
         <Menu.Item
           onClick={(event) => {
             event.stopPropagation();
